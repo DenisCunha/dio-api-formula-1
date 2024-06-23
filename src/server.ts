ee1d56d69e0a233999e089506c264c182e8347d8
deniscunha <ded_2004@hotmail.com>
2024-06-23 10:41:16 -0300

    insert teams and drivers

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,29 @@ import fastify from "fastify";
 
 const server = fastify({logger: true});
 
+const teams = [
+{id: 1, name: "MacLaren", base: "United Kingdom"},
+{id: 2, name: "Mercedes", base: "United Kingdom"},
+{id: 3, name: "Red Bull Racing", base: "United Kingdom"},
+];
+
+const drivers = [
+    {id: 1, name: "Max Vertappen", team: "Red Bull Racing"},
+    {id: 2, name: "Lewis Hamilton", team: "Ferrari"},
+    {id: 3, name: "Lando Norris", team: "MacLaren"},
+    ];
+    
+
 server.get("/teams", async (request, response) => {
 response.type("application/json").code(200);
-return [{id: 1, name: "ferrari"}];
-}
-);
-
+return {teams};
+});
 
 server.listen({port: 3333}, () => {
 console.log("Server Init");
+});
+
+server.get("/drivers", async(request, response) =>  {
+response.type("application/json").code(200);
+return {drivers};
 });
